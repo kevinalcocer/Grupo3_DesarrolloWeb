ead37619b5b385beb2d8f4c2969a2dd6b8b0de07
Kevin Alcocer <kaalcocer@espe.edu.ec>
2023-06-27 18:06:13 -0400

    Se elimina la carpeta

--- 06-Code/02ServidorWeb/PANADERIA/backend/helpers/generarJWT.js
+++
@@ -1,9 +0,0 @@
-
-import jwt from 'jsonwebtoken';
-const generarJWT = (id) =>{
-    return jwt.sign({id},"juan",{  //se usa una palabra clave "juan"
-        expiresIn: "30d",
-    }  )
-};
-
-export default generarJWT;
--- 06-Code/02ServidorWeb/PANADERIA/backend/helpers/generaris.js
+++
@@ -1,7 +0,0 @@
-const generarIdCompleja = () =>{
-    return Date.now().toString(32)+Math.random().toString(32).substring(2);
-}
-const generarId = () =>{
-    return Date.now().toString(32);
-}
-export default generarId;
